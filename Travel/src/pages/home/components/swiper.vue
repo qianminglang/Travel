cc7229d139d2f9e821bedaae8f14002b40ed533c
qianminglang <569040804@qq.com>
2018-12-04 21:36:34 +0800

    首页轮播图

--- Travel/src/pages/home/components/swiper.vue
+++ Travel/src/pages/home/components/swiper.vue
@@ -18,7 +18,7 @@ export default {
         pagination: '.swiper-pagination',
         loop: true
       },
-      swiperList:[
+      swiperList: [
         {
           id: '0001',
           imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/29/2a6b77b6ff72402.jpg_750x200_d4cec568.jpg'
@@ -36,10 +36,10 @@ export default {
 <style lang="stylus" scoped>
   /*因为下面的这个样式是在home页面设置的，如果是常规的写法我们修改不了样式，
     下面写法的意思是wrapdom元素的子元素中有swiper-pagination-bullet-active
-    类的修饰样式都进行穿透，那不是本页面scoped范围的样式可以穿透
+    类的修饰样式都进行穿透，那不是本页面scoped范围的样式可以穿透分支是，这个时候dev分支
   */
   .wraper >>> .swiper-pagination-bullet-active
-    background: #fff
+    background: #ffffff
   .wraper
     width 100%
     height 0
