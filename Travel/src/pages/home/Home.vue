9b6145dc10d621486bf9fe366fc589395f42b9fc
qianminglang <569040804@qq.com>
2018-12-05 23:11:50 +0800

    add icons

--- Travel/src/pages/home/Home.vue
+++ Travel/src/pages/home/Home.vue
@@ -2,18 +2,21 @@
   <div>
     <home-header></home-header>
     <home-swiper></home-swiper>
+    <icons></icons>
   </div>
 </template>
 
 <script>
 import homeHeader from './components/header'
 import HomeSwiper from './components/swiper'
+import Icons from './components/icons'
 
 export default {
   name: 'home',
   components: {
     homeHeader,
-    HomeSwiper
+    HomeSwiper,
+    Icons
   }
 }
 </script>
---
+++ Travel/src/pages/home/components/icons.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="icons">
+    <swiper>
+      <swiper-slide v-for="(page) of pages" >
+        <div class="icon" v-for="(item) of page" :key="item.id">
+          <div class="image-icon">
+            <img class="image-content" :src="item.imgUrl" alt=""/>
+          </div>
+          <p class="icon-desc">
+            {{item.desc}}
+          </p>
+        </div>
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'icons',
+  data () {
+    return {
+      iconList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '景点门票多字体检测呢'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/ab/6f7d6e44963c9302.png',
+          desc: '泡温泉'
+        },
+        {
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/e3/67df61427c8e1302.png',
+          desc: '云南映象'
+        },
+        {
+          id: '0004',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/96/c70f1e85ae4a4f02.png',
+          desc: '石林'
+        },
+        {
+          id: '0005',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+          desc: '昆明必游'
+        },
+        {
+          id: '0006',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/5e/c640ec3b7d7ae802.png',
+          desc: '打卡圣地'
+        },
+        {
+          id: '0007',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/87/20656da0ac042002.png',
+          desc: '九乡风景区'
+        },
+        {
+          id: '0008',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/b1/528a9e80403b8c02.png',
+          desc: '七彩云南'
+        },
+        {
+          id: '0009',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+          desc: '一日游'
+        }
+      ]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/varibles.styl"
+  @import "~styles/mixmins.styl"
+  .icons>>> .swiper-container
+    overflow: hidden
+    height 0
+    padding-bottom 50%
+    .icon
+      position relative
+      overflow hidden
+      width 25%
+      float left
+      padding-bottom 25%
+      .image-icon
+        position absolute
+        top:0
+        left 0
+        right 0
+        bottom 0.44rem
+        box-sizing border-box
+        padding .1rem
+        .image-content
+          height 100%
+          display block
+          margin 0 auto
+      .icon-desc
+        position absolute
+        left 0
+        right 0
+        bottom 0
+        height 0.44rem
+        line-height .44rem
+        text-align center
+        color: $dareTextColor
+        ellipsis()
+</style>
