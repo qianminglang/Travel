a8479be39c28a15df80192d53c2d05d1f4be5662
qianminglang <569040804@qq.com>
2018-12-08 19:48:21 +0800

    city-search

--- Travel/src/pages/city/city.vue
+++ Travel/src/pages/city/city.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <city-header></city-header>
+    <city-search></city-search>
   </div>
 </template>
 
 <script>
 import cityHeader from './components/header'
+import citySearch from './components/search'
 export default {
   name: 'list',
   components: {
-    cityHeader
+    cityHeader,
+    citySearch
   }
 }
 </script>
